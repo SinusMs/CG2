155ce364871bb7aea968a9a8a2358051091637ea
Benjamin Russig <benjamin.russig@tu-dresden.de>
2024-04-10 18:31:44 +0200

    Update Windows binary reference solutions for SS2024

--- solution/run_exercise1.bat
+++ solution/run_exercise1.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 cd ..
-.\solution\bin\windows\cgv_viewer64_143.exe plugin:cg_fltk "type(shader_config):shader_path='%cd%\solution\glsl'" plugin:crg_stereo_view plugin:cg_icons plugin:cg_ext plugin:cmi_io plugin:CG2_exercise1 "config:'%cd%\solution\config_ex1.cfg'"
+.\solution\bin\windows\cgv_viewer64_143.exe plugin:cg_fltk "type(shader_config):shader_path='%cd%\solution\glsl'" plugin:cmf_tt_gl_font plugin:cg_ext plugin:cg_meta plugin:cmi_io plugin:crg_stereo_view plugin:CG2_exercise1 "config:'%cd%\solution\config_ex1.cfg'"
--- solution/run_exercise45.bat
+++ solution/run_exercise45.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 cd ..
-.\solution\bin\windows\cgv_viewer64_143.exe plugin:cg_fltk "type(shader_config):shader_path='%cd%\solution\glsl'" plugin:crg_stereo_view plugin:cg_icons plugin:cg_ext plugin:cmi_io plugin:CG2_exercise45 "config:'%cd%\solution\config_ex45.cfg'"
+.\solution\bin\windows\cgv_viewer64_143.exe plugin:cg_fltk "type(shader_config):shader_path='%cd%\solution\glsl'" plugin:cg_ext plugin:cg_meta plugin:cmi_io plugin:crg_stereo_view plugin:CG2_exercise45 "config:'%cd%\solution\config_ex45.cfg'"
