1d5621e79db337d1611edc8f43ebd415995b5d18
Marc Scheibner <marc.scheibner1@mailbox.tu-dresden.de>
2024-04-27 22:13:10 +0200

    0.3.1 mostly implemented (normals and color not working)

--- exercise0/cubes_drawable.cxx
+++ exercise0/cubes_drawable.cxx
@@ -44,7 +44,7 @@ protected:
 	float r, g, b;
 	cgv::rgb color;
 	unsigned max_depth;
-	enum DrawingMode { BUILTIN, INTERLEAVED, NONINTERLEAVED } drawing_mode;
+	enum DrawingMode { BUILTIN, INTERLEAVED, NONINTERLEAVED, SINGLE_VERTEX_BUFFER } drawing_mode;
 
 	struct vertex {
 		cgv::vec3 pos;
@@ -60,8 +60,19 @@ protected:
 	cgv::render::vertex_buffer vb_normals;
 	cgv::render::attribute_array_binding vertex_array_noninterleaved;
 
+	bool fractal_dirty;
+	struct fractal_vertex {
+		fractal_vertex(vertex v, cgv::media::color<float> color) : pos(v.pos), normal(v.normal), color(color) {}
+		cgv::vec3 pos;
+		cgv::vec3 normal;
+		cgv::media::color<float> color;
+	};
+	std::vector<fractal_vertex> fractal_vertices;
+	cgv::render::vertex_buffer vb_fractal;
+	cgv::render::attribute_array_binding vertex_array_fractal;
+
 public:
-	cubes_drawable() : enable(false), r(1.0), g(1.0), b(1.0), color(r, g, b), max_depth(3), drawing_mode(NONINTERLEAVED) { }
+	cubes_drawable() : enable(false), r(1.0), g(1.0), b(1.0), color(r, g, b), max_depth(3), drawing_mode(NONINTERLEAVED), fractal_dirty(true) { }
 
 	std::string get_type_name(void) const
 	{
@@ -82,6 +93,7 @@ public:
 
 	void on_set(void* member_ptr)
 	{
+		if (drawing_mode == SINGLE_VERTEX_BUFFER) fractal_dirty = true;
 		if (member_ptr == &r || member_ptr == &g ||	member_ptr == &b)
 		{
 			color.R() = r;
@@ -105,7 +117,7 @@ public:
 		add_member_control(this, "Enable", enable);
 		add_member_control(this, "Color", color);
 		add_member_control(this, "Max Depth", max_depth);
-		add_member_control(this, "Drawing Mode", drawing_mode, "dropdown", "enums='BUILTIN, INTERLEAVED, NONINTERLEAVED'");
+		add_member_control(this, "Drawing Mode", drawing_mode, "dropdown", "enums='BUILTIN, INTERLEAVED, NONINTERLEAVED, SINGLE_VERTEX_BUFFER'");
 	}
 
 	bool init(cgv::render::context& ctx)
@@ -125,7 +137,7 @@ public:
 		success = vertex_array_noninterleaved.create(ctx) && success;
 		success = vertex_array_noninterleaved.set_attribute_array(ctx, ctx.ref_surface_shader_program().get_position_index(), vec3type, vb_positions, 0, vertex_positions.size(), sizeof(cgv::vec3)) && success;
 		success = vertex_array_noninterleaved.set_attribute_array(ctx, ctx.ref_surface_shader_program().get_normal_index(), vec3type, vb_normals, 0, vertex_normals.size(), sizeof(cgv::vec3)) && success;
-		
+
 		return success;
 	}
 
@@ -141,16 +153,26 @@ public:
 		switch (drawing_mode) {
 			case INTERLEAVED:
 				fractal.use_vertex_array(&vertex_array_interleaved, vertices_interleaved.size(), GL_QUADS);
+				fractal.draw_recursive(ctx, color, max_depth);
 				break;
 			case NONINTERLEAVED:
 				fractal.use_vertex_array(&vertex_array_noninterleaved, vertex_positions.size(), GL_QUADS);
+				fractal.draw_recursive(ctx, color, max_depth);
+				break;
+			case SINGLE_VERTEX_BUFFER:
+				if (fractal_dirty) {
+					fractal_dirty = false;
+					init_fractal_vao(ctx, color);
+				}
+				vertex_array_fractal.enable(ctx);
+				glDrawArrays(GL_QUADS, 0, fractal_vertices.size());
+				vertex_array_fractal.disable(ctx);
 				break;
 			case BUILTIN:
 			default:
+				fractal.draw_recursive(ctx, color, max_depth);
 				break;
 		}
-
-		fractal.draw_recursive(ctx, color, max_depth);
 
 		default_shader.disable(ctx);
 	}
@@ -193,6 +215,51 @@ public:
 		for (int i = 0; i < vertices_interleaved.size(); i++) {
 			vertex_positions.push_back(vertices_interleaved[i].pos);
 			vertex_normals.push_back(vertices_interleaved[i].normal);
+		}
+	}
+
+	void init_fractal_vao(cgv::render::context& ctx, const cgv::media::color<float>& color) {
+		fractal_vertices.clear();
+		init_fractal_vao_rec(color);
+		for each (fractal_vertex v in fractal_vertices) {
+			std::cout << "Pos: " << v.pos << std::endl;
+			std::cout << "Nor: " << v.normal << std::endl;
+			std::cout << "Col: " << v.color << std::endl << std::endl;
+		}
+
+		cgv::render::type_descriptor 
+			vec3type = cgv::render::element_descriptor_traits<cgv::vec3>::get_type_descriptor(fractal_vertices[0].pos),
+			coltype = cgv::render::element_descriptor_traits<cgv::media::color<float>>::get_type_descriptor(fractal_vertices[0].color);
+
+		vb_fractal.create(ctx, &(fractal_vertices[0]), fractal_vertices.size());
+
+		vertex_array_fractal.create(ctx);
+		vertex_array_fractal.set_attribute_array(ctx, ctx.ref_surface_shader_program().get_position_index(), vec3type, vb_fractal, 0, fractal_vertices.size(), sizeof(fractal_vertex));
+		vertex_array_fractal.set_attribute_array(ctx, ctx.ref_surface_shader_program().get_normal_index(), vec3type, vb_fractal, sizeof(cgv::vec3), fractal_vertices.size(), sizeof(fractal_vertex));
+		vertex_array_fractal.set_attribute_array(ctx, ctx.ref_surface_shader_program().get_color_index(), coltype, vb_fractal, sizeof(cgv::vec3) * 2, fractal_vertices.size(), sizeof(fractal_vertex));
+	}
+
+	void init_fractal_vao_rec(const cgv::media::color<float>& color, cgv::dmat4 mat = cgv::math::scale4<double>(1, 1, 1), unsigned level = 0)
+	{
+		//std::cout << mat << std::endl << std::endl;
+		mat *= cgv::math::scale4<double>(.5, .5, .5);
+		//std::cout << cgv::math::scale4<double>(1, 1, 1) * cgv::math::scale4<double>(.5, .5, .5) << std::endl;
+
+		for each (vertex v in vertices_interleaved)
+		{
+			fractal_vertex fv = fractal_vertex(v, color);
+			fv.pos = mat.mul_pos(fv.pos);
+			fractal_vertices.push_back(fv);
+		}
+
+		cgv::media::color<float, cgv::media::HLS> color_next(color);
+		color_next.H() = std::fmod(color_next.H() + 0.2f, 1.0f);
+		color_next.S() = std::fmod(color_next.S() - 0.05f, 1.0f);
+
+		unsigned num_children = level == 0 ? 4 : 3;
+		if (level < max_depth) for (unsigned i = 0; i < num_children; i++)
+		{
+			init_fractal_vao_rec(color_next, mat * cgv::math::rotate4<double>(signed(i) * 90 - 90, 0, 0, 1) * cgv::math::translate4<double>(2, 0, 0), level + 1);
 		}
 	}
 };
