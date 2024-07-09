8a0b7f19463d813597629dd32e9061a17ce22b8d
HPLaptop <Hans.Kraschewski@mailbox.tu-dresden.de>
2024-07-09 14:58:12 +0200

    task4.1 not working

--- exercise34/Bone.cxx
+++ exercise34/Bone.cxx
@@ -28,17 +28,27 @@ Bone::~Bone()
 
 void Bone::calculate_matrices()
 {
+
 	orientationSystemTransformLocalToGlobal.identity();
 	std::for_each(orientation.begin(), orientation.end(), [&](AtomicTransform* t) {
-        orientationSystemTransformLocalToGlobal = orientationSystemTransformLocalToGlobal * t->calculate_matrix();
-	});
-    orientationModelTransformLocalToGlobal = cgv::math::inv(orientationSystemTransformLocalToGlobal);
-    // ^ same thing:  orientationSystemTransformGlobalToLocal = cgv::math::inv(orientationModelTransformGlobalToLocal);
+		orientationSystemTransformLocalToGlobal = orientationSystemTransformLocalToGlobal * t->calculate_matrix();
+		});
+	orientationModelTransformLocalToGlobal = cgv::math::inv(orientationSystemTransformLocalToGlobal);
+	// ^ same thing:  orientationSystemTransformGlobalToLocal = cgv::math::inv(orientationModelTransformGlobalToLocal);
 
-	////
-	// Task 3.1: Implement matrix calculation
-
-
+	Vec4 globalDirection = Vec4(direction_in_world_space.x() * length, direction_in_world_space.y() * length, direction_in_world_space.z() * length, 0);
+	Vec4 localDirection = orientationSystemTransformGlobalToLocal * globalDirection;
+	translationModelTransformCurJointToNext = translate(localDirection.x(), localDirection.y(), localDirection.z());
+	if (parent != nullptr)
+	{
+		orientationModelTransformPrevJointToCur = parent->orientationModelTransformLocalToGlobal * orientationModelTransformGlobalToLocal;
+		translationSystemTransformLocalToGlobal = parent->translationSystemTransformLocalToGlobal * translate(parent->direction_in_world_space * parent->length);
+	}
+	else
+	{
+		orientationModelTransformPrevJointToCur = orientationSystemTransformLocalToGlobal;
+		translationSystemTransformLocalToGlobal.identity();
+	}
 
 	////
 	// Task 4.6: Implement matrix calculation (skinning)
@@ -47,19 +57,26 @@ void Bone::calculate_matrices()
 
 Mat4 Bone::calculate_transform_prev_to_current_with_dofs()
 {
-	////
-	// Task 3.1: Implement matrix calculation
-
-	Mat4 t;
+	// Calculates (T_prev2cur * O_prev2cur) * DoF_cur
+		//
+		//	T.....translation matrix (e.g. T_prev translation from previous bone to current)
+		//	O....."orientation" matrix, i.e. an arbitrary rotation of the local coordinate system relative to the parent. It is chosen such that we can define our degrees of freedom as axis-aligned Euler angles
+		//	DoF...degrees-of-freedom matrix - typically just a combination of rotations, ASF files use 1 to 3 Euler angles
+	Mat4 t = calculate_transform_prev_to_current_without_dofs();
+	for (unsigned int i = 0; i < dofs.size(); ++i)
+		t = t * dofs[i]->calculate_matrix();
 	return t;
 }
 
 Mat4 Bone::calculate_transform_prev_to_current_without_dofs()
 {
-	////
-	// Task 3.1: Implement matrix calculation
-
-	Mat4 t;
+	// Calculates T_prev2cur * O_prev2cur
+		//
+		//	T.....translation matrix (e.g. T_prev translation from previous bone to current)
+		//	O....."orientation" matrix, i.e. an arbitrary rotation of the local coordinate system relative to the parent. It is chosen such that we can define our degrees of freedom as axis-aligned Euler angles
+	Mat4 t = orientationModelTransformPrevJointToCur;
+	if (parent != nullptr)
+		t = parent->translationModelTransformCurJointToNext * t;
 	return t;
 }
 
--- exercise34/Skeleton.cxx
+++ exercise34/Skeleton.cxx
@@ -268,6 +268,92 @@ void Skeleton::write_pinocchio_file(const std::string& filename)
 
 	if (o)
 	{
+		Bone* root = Skeleton::get_root();
+		std::list<Bone*> to_visit;
+		std::list<Vec3> global_positions;
+		std::list<int> ids;
+		std::list<int> parent_ids;
+		int id = -1;
+
+		// o << 0 << " " << Vec3(root->get_bone_local_tip_position()) << -1 << std::endl;
+		for (size_t i = 0; i < root->childCount(); i++)
+		{
+			auto child = root->child_at(i);
+			to_visit.push_front(child);
+			parent_ids.push_front(id);
+		}
+
+		
+		// read bone information out of hierarchy and store in lists, depth first
+		while (to_visit.size() != 0)
+		{
+			auto current_bone = to_visit.front();
+			to_visit.pop_front();
+			parent_ids.push_back(parent_ids.front());
+			parent_ids.pop_front();
+			id++;
+
+			
+			// add children to list
+			for (size_t i = 0; i < current_bone->childCount(); i++)
+			{
+				auto child = current_bone->child_at(i);
+				to_visit.push_front(child);
+				parent_ids.push_front(id);
+			}
+			
+			//calc global bone position
+			Vec4 lokal_pos = current_bone->get_bone_local_tip_position();
+			Vec4 global_pos = current_bone->calculate_transform_prev_to_current_with_dofs() * lokal_pos;
+			auto next = current_bone->get_parent();
+			while (next->get_parent() != nullptr) {
+				global_pos = next->calculate_transform_prev_to_current_without_dofs() * global_pos;
+				next = next->get_parent();
+			}
+
+			// write current_bones information into lists file
+			ids.push_back(id);
+			global_positions.push_back(Vec3(global_pos));
+			
+		}
+
+		//calculate maxima
+		Vec3 max, min;
+		max = Vec3(0, 0, 0);
+		min = Vec3(0, 0, 0);
+		for (std::list<Vec3>::iterator it = global_positions.begin(); it != global_positions.end(); ++it) {
+			max.x() = max.x() < it->x() ? it->x() : max.x();
+			max.y() = max.y() < it->y() ? it->y() : max.y();
+			max.z() = max.z() < it->z() ? it->z() : max.z();
+
+			min.x() = min.x() > it->x() ? it->x() : min.x();
+			min.y() = min.y() > it->y() ? it->y() : min.y();
+			min.z() = min.z() > it->z() ? it->z() : min.z();
+		}
+
+		//calculate max expansion
+		float size = max.x() - min.x();
+		size = size < (max.y() - min.y()) ? (max.y() - min.y()) : size;
+		size = size < (max.z() - min.z()) ? (max.z() - min.z()) : size;
+
+
+		std::cout << parent_ids.size();
+		std::cout << global_positions.size();
+		// write into pino file
+		int n = global_positions.size();
+		for (size_t i = 0; i < n; i++)
+		{
+			Vec3 current_pos = global_positions.front() + cgv::math::abs(min);
+			o << ids.front() << " " << current_pos/size << " " << parent_ids.front() << std::endl;
+			ids.pop_front();
+			global_positions.pop_front();
+			parent_ids.pop_front();
+		}
+
+
+		
+	
+
 		/*Task 4.1: Write Pinocchio file into o */
 	}
 	o.close();
